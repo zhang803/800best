dde1db9a315ce3fff9016f2a82d71400ebd3641a
zhang803 <80885861+zhang803@users.noreply.github.com>
2023-04-11 16:11:27 +0800

    Update config.py
    修改配置，从服务端调用

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 # encoding:utf8
 # 
 # code by 800best
+# 百世生活OA调用接口
 
-key = 'bestbest'
-domain = 'oa.800best.com'
-username = 'admin'
-password = 'New8oobest.cOm!'
+var app = {
+  key: 'oakey800best@com'
+  domain: 'oa.800best.life'
+  loginUrl: '/best/oa'
+  username: 'best_oa'
+  password: 'Abc2467#123'
+  bid:'oapi_0x5f3759df_v1_cli_a36ae2528df0900b'
+  apidev: 'http://oa.800best.life/best/oa'
+}  
+
