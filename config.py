7f65810f7517efa69250ba443354508b542a1569
zhang803 <80885861+zhang803@users.noreply.github.com>
2023-06-08 18:00:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 # encoding:utf8
 # 
-# code by 800best
 # 百世生活OA调用接口
+#
+# code by Zhang.lock@800best.com
+#
 
 var app = {
   key: 'oakey800best@com'
+  # domain: 'oa.800best.com'
   domain: 'oa.800best.life'
   loginUrl: '/best/oa'
   username: 'best_oa'
-  password: 'Abc2467#123'
+  password: 'your_password' # password: 'Abc2467#123'
   bid:'oapi_0x5f3759df_v1_cli_a36ae2528df0900b'
   apidev: 'http://oa.800best.life/best/oa'
 }  
